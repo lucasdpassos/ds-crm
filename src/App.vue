ebed97322605546c2ab4acf54a4ddf9a3bc80b39
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-08-17 18:35:09 -0300

    staging branche creation

--- src/App.vue
+++ src/App.vue
@@ -4,53 +4,25 @@
       app
       color="primary"
       dark
+      style="display:flex;flex-direction:row;justify-content:center;"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar-title>Tem De Tudo</v-app-bar-title>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+    <router-view></router-view>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+   //
   },
 
   data: () => ({
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    
+    <v-btn>Produtos</v-btn>
+    <v-btn>Clientes</v-btn>
+    <v-btn>Vendas</v-btn>
+   
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+   //
+  }
+}
+</script>
