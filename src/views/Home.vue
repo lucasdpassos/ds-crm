6e805e2c7afd26cc464133acb0fc763adc3013a0
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-08-18 17:35:40 -0300

    products table and insert product

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     
     <div style="display:flex;flex-direction:row;justify-content:space-around;height:500px;align-items:center;">
-    <v-btn color="primary">Produtos</v-btn>
+    <v-btn @click="$router.push('products')" color="primary">Produtos</v-btn>
     <v-btn color="primary">Clientes</v-btn>
     <v-btn color="primary">Vendas</v-btn>
    </div> 
